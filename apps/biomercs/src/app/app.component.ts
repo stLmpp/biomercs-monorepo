cb4338755986309eced582ad38cb70693df57aaa
stLmpp <gui.stlmpp@hotmail.com>
2020-12-17 09:31:25 -0300

    initial commit

--- apps/biomercs/src/app/app.component.ts
+++ apps/biomercs/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@biomercs/api-interfaces';
 
 @Component({
-  selector: 'biomercs-root',
+  selector: 'bio-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   hello$ = this.http.get<Message>('/api/hello');
