5db329b0b923d5f95af26e81abf0eff63f29ee5e
stLmpp <gui.stlmpp@hotmail.com>
2020-12-18 09:22:32 -0300

    fix(api): explicitly add entities to the db.config update(): update dependencies

--- apps/api/src/app/environment/db.config.ts
+++ apps/api/src/app/environment/db.config.ts
@@ -1,21 +1,54 @@
 import { NamingStategy } from './naming.strategy';
 import { environment } from './environment';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-const contexts = (require as any).context('../../', true, /\.entity.ts$/);
-const entities = contexts
-  .keys()
-  .map((modulePath: any) => contexts(modulePath))
-  .reduce(
-    (result: any[], entityModule: { [x: string]: any }) =>
-      result.concat(Object.keys(entityModule).map(key => entityModule[key])),
-    []
-  );
+import { CharacterCostume } from '../character/character-costume/character-costume.entity';
+import { Player } from '../player/player.entity';
+import { GameMiniGame } from '../game/game-mini-game/game-mini-game.entity';
+import { User } from '../user/user.entity';
+import { Character } from '../character/character.entity';
+import { ScorePlayer } from '../score/score-player/score-player.entity';
+import { Mode } from '../mode/mode.entity';
+import { AuthConfirmation } from '../auth/auth-confirmation/auth-confirmation.entity';
+import { PlatformGameMiniGameModeCharacterCostume } from '../platform/platform-game-mini-game-mode-character-costume/platform-game-mini-game-mode-character-costume.entity';
+import { MiniGame } from '../mini-game/mini-game.entity';
+import { ScoreApprovalMotive } from '../score/score-approval-motive/score-approval-motive.entity';
+import { SteamProfile } from '../steam/steam-profile.entity';
+import { Score } from '../score/score.entity';
+import { Game } from '../game/game.entity';
+import { PlatformGameMiniGameMode } from '../platform/platform-game-mini-game-mode/platform-game-mini-game-mode.entity';
+import { PlatformGameMiniGame } from '../platform/platform-game-mini-game/platform-game-mini-game.entity';
+import { PlatformGameMiniGameModeStage } from '../platform/platform-game-mini-game-mode-stage/platform-game-mini-game-mode-stage.entity';
+import { Region } from '../region/region.entity';
+import { Stage } from '../stage/stage.entity';
+import { ScoreApproval } from '../score/score-approval/score-approval.entity';
+import { Platform } from '../platform/platform.entity';
 
 export const DB_TYPEORM_CONFIG: TypeOrmModuleOptions = {
   ...environment.database,
   type: 'mysql',
-  entities,
+  entities: [
+    AuthConfirmation,
+    CharacterCostume,
+    Character,
+    GameMiniGame,
+    Game,
+    MiniGame,
+    Mode,
+    PlatformGameMiniGameModeCharacterCostume,
+    PlatformGameMiniGameModeStage,
+    PlatformGameMiniGameMode,
+    PlatformGameMiniGame,
+    Platform,
+    Player,
+    Region,
+    ScoreApprovalMotive,
+    ScoreApproval,
+    ScorePlayer,
+    Score,
+    Stage,
+    SteamProfile,
+    User,
+  ],
   logging: !environment.production ? 'all' : false,
   bigNumberStrings: false,
   namingStrategy: new NamingStategy(),
